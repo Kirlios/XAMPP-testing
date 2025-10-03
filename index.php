8a85bf334ee174ec1c555548754ff591b0cfb2c7
Tomáš Vallo <t.vallo444@gmail.com>
2025-10-03 12:48:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include('./config.php');?>
+<!DOCTYPE html>
+<html>
+
+</head>
+<link rel='stylesheet' type='text/css' href='./assets/css/root.css'>
+<hr>
+<body>
+ <header>
+   <div class='header-logo'>
+       <img src='./assets/imgs/logo.png'>
+       <img src='./assets/imgs/kok.jpg'>
+   </div>
+<?php
+
+?>
+
+<h1><?php echo (SITE_NAME); ?></h1>
+<p><?php echo (SITE_AUTHOR); ?></p>
+<P><?PHP echo (SITE_NAME2); ?></p>
+</body>
+</html>
