ce70510b8801091ffe92c3bdb58e1f4365de07f6
Tomáš Vallo <t.vallo444@gmail.com>
2025-10-07 11:12:13 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SITE_NAME','paralen je len jeden...');
-define('SITE_AUTHOR','ale plesatych nie len jeden, ba aj dva...');
-define('SITE_URL','www.dohlavy.sk');
-define('SITE_NAME2','Alopecia je lekársky termín pre plešatosť alebo stratu ochlpenia na tele, ktorá môže mať rôzne príčiny, vrátane genetiky, stresu, autoimunitných reakcií, hormonálnych zmien alebo nežiaducich účinkov liekov');
-?>
