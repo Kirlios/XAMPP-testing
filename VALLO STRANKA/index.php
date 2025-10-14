a8c51eb8696843265120e7bb4a4e766d534f49f3
Tomáš Vallo <t.vallo444@gmail.com>
2025-10-14 10:07:24 +0200

    Add files via upload

--- VALLO STRANKA/index.php
+++ VALLO STRANKA/index.php
@@ -1,1 +1,18 @@
+<?php include('./config.php');?>
+<!DOCTYPE html>
+<html>
 
+</head>
+<link rel='stylesheet' type='text/css' href='./assets/css/root.css'>
+
+<body>
+ 
+<?php
+
+?>
+
+<h1><?php echo (SITE_NAME); ?></h1>
+<p><?php echo (SITE_AUTHOR); ?></p>
+
+</body>
+</html>
